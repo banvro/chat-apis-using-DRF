51636d11400abb8a9e8c2a87d0f51f4f5ed518c7
ubuntonaresh <ubuntonaresh@gmail.com>
2023-04-26 12:05:09 +0530

    create model

--- APIs/serializers.py
+++ APIs/serializers.py
@@ -1,20 +1,20 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
+# from rest_framework import serializers
+# from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username']
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username']
 
-class ChatRoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    participants = serializers.ListField(child=serializers.IntegerField())
+# class ChatRoomSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=255)
+#     participants = serializers.ListField(child=serializers.IntegerField())
 
-class MessageSerializer(serializers.Serializer):
-    author = UserSerializer()
-    content = serializers.CharField()
-    timestamp = serializers.DateTimeField()
+# class MessageSerializer(serializers.Serializer):
+#     author = UserSerializer()
+#     content = serializers.CharField()
+#     timestamp = serializers.DateTimeField()
 
-class ChatRoomMessageSerializer(serializers.Serializer):
-    chat_room = ChatRoomSerializer()
-    message = MessageSerializer()
+# class ChatRoomMessageSerializer(serializers.Serializer):
+#     chat_room = ChatRoomSerializer()
+#     message = MessageSerializer()
--- APIs/urls.py
+++ APIs/urls.py
@@ -1,8 +1,6 @@
 # urls.py
 from django.urls import path
-from .views import ChatRoomAPIView, ChatRoomWebSocketConsumer
 
 urlpatterns = [
-    path('chat_rooms/', ChatRoomAPIView.as_view()),
-    path('chat_rooms/<int:chat_room_id>/', ChatRoomWebSocketConsumer.as_asgi()),
+    
 ]
