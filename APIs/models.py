d97f2a591db0e600d4cb643a70ec3303aadd2f7c
ubuntonaresh <ubuntonaresh@gmail.com>
2023-04-26 12:00:38 +0530

    create user profile

--- APIs/models.py
+++ APIs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+  
+class User(models.Model):
+    uid = models.UUIDField(primary_key = True, unique=True, default = uuid.uuid4, editable = False)
+    username = models.CharField(max_length=250, unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(unique=True,max_length=30)
+    DateOfBirth = models.CharField(max_length=70)
+    firebase_password = models.CharField(max_length=250, null=True, blank=True)
+    firebase_uid = models.CharField(max_length=255, null=True, blank=True)
+    profilepic = models.ImageField(upload_to='ProfilePic', null=True, blank=True)
