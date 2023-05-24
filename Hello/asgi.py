5e833d3db81540c90ea613ebd3b76f2483e6053a
ubuntonaresh <ubuntonaresh@gmail.com>
2023-05-24 15:49:30 +0530

    Change ASgi

--- Hello/asgi.py
+++ Hello/asgi.py
@@ -1,11 +1,40 @@
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
 from hlo.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hello.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hello.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": URLRouter(websocket_urlpatterns),
+# })
+
+
+
+
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+from django.urls import path
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Hello.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
 })
